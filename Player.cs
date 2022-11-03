b4b7a8f01680b3ec22d0c1192c31288004fbd487
Liam Neville <lineville@github.com>
2022-11-03 16:25:09 -0700

    wip

---
+++ Player.cs
@@ -0,0 +1,6 @@
+public record Player {
+  public string? Name { get; init; }
+  public int NationalRank { get; init; }
+  public int SectionRank { get; init; }
+  public int DistrictRank { get; init; }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Safari;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using Microsoft.Extensions.Configuration;
 using System.Web;
+using Newtonsoft.Json;
+
+// TODO setup github action to run this every week, get the results and update the value in my readme
+
+// TODO Or create a badge that shows the current rank with link!!!
 
 // Read in config values for appsettings.json to construct the URL
 var config = new ConfigurationBuilder()
@@ -11,10 +16,39 @@ var config = new ConfigurationBuilder()
                 .Build();
 
 var queryParams = config.GetSection("UserConfig").Get<Dictionary<string, string>>();
-var url = $"https://www.usta.com/en/home/play/rankings.html#?ntrp-matchFormat={queryParams["ntrp-matchFormat"]}&ntrp-rankListGender={queryParams["ntrp-rankListGender"]}&ntrp-ntrpPlayerLevel={queryParams["ntrp-ntrpPlayerLevel"]}&ntrp-sectionCode={queryParams["ntrp-sectionCode"]}&searchText={HttpUtility.UrlEncode(queryParams["searchText"])}#tab=ntrp";
 
-// Launch headless browser, go to url and wait for it to be ready
-var driver = new SafariDriver();
+// TODO -- it would be nice to just pass in the whole dictionary using QueryHelpers , but it omits the # after rankings.html ... so I have to manually add it
+var url = @$"https://www.usta.com/en/home/play/rankings.html#?
+ntrp-matchFormat={queryParams["ntrp-matchFormat"]}&
+ntrp-rankListGender={queryParams["ntrp-rankListGender"]}&
+ntrp-ntrpPlayerLevel={queryParams["ntrp-ntrpPlayerLevel"]}&
+ntrp-sectionCode={queryParams["ntrp-sectionCode"]}&
+searchText={HttpUtility.UrlEncode(queryParams["searchText"])}#tab=ntrp";
+
+// Setup headless chrome driver and wait for the page to load
+FirefoxDriverService service = FirefoxDriverService.CreateDefaultService();
+service.LogLevel = FirefoxDriverLogLevel.Fatal;
+service.SuppressInitialDiagnosticInformation = true;
+service.HideCommandPromptWindow = true;
+
+FirefoxOptions options = new FirefoxOptions();
+
+options.PageLoadStrategy = PageLoadStrategy.Normal;
+
+options.AddArgument("--window-size=1920,1080");
+options.AddArgument("--no-sandbox");
+options.AddArgument("--headless");
+options.AddArgument("--disable-gpu");
+options.AddArgument("--disable-crash-reporter");
+options.AddArgument("--disable-extensions");
+options.AddArgument("--disable-in-process-stack-traces");
+options.AddArgument("--disable-logging");
+options.AddArgument("--disable-dev-shm-usage");
+options.AddArgument("--log-level=3");
+options.AddArgument("--output=/dev/null");
+
+var driver = new FirefoxDriver(service, options);
+
 driver.Navigate().GoToUrl(url);
 WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 1, 0));
 
@@ -29,9 +63,16 @@ var sectionRank = elements[1].Text;
 var districtRank = elements[2].Text;
 var name = elements[3].Text;
 
-Console.WriteLine($"{name}\n-------------------------\n");
-Console.WriteLine($"National Rank: #{nationalRank}");
-Console.WriteLine($"Section Rank: #{sectionRank}");
-Console.WriteLine($"District Rank: #{districtRank}");
+var player = new Player()
+{
+  Name = elements[3].Text,
+  NationalRank = int.Parse(elements[0].Text),
+  SectionRank = int.Parse(elements[1].Text),
+  DistrictRank = int.Parse(elements[2].Text)
+};
+
+string jsonString = JsonConvert.SerializeObject(player, Formatting.Indented);
+
+Console.WriteLine(jsonString);
 
 driver.Quit();
