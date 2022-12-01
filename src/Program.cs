cf3efbdc9782d377bc16587a15b2791d3dc08849
Liam Neville <lineville@github.com>
2022-12-01 12:35:47 -0800

    add user agent fix for headless

--- src/Program.cs
+++ src/Program.cs
@@ -82,6 +82,7 @@ public class Program
       "window-size=1920,1080",
       "disable-extensions",
       "log-level=OFF",
+      "--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
       "output=/dev/null"
     );
 
