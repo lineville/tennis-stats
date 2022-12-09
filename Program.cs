c71cb472262837e73655902086e63bdd20e5d296
Liam Neville <lineville@github.com>
2022-12-08 16:35:42 -0800

    put options back

--- Program.cs
+++ Program.cs
@@ -42,7 +42,18 @@ public class Program
     service.SuppressInitialDiagnosticInformation = true;
 
     ChromeOptions options = new ChromeOptions();
-    options.AddArguments("headless", "--user-agent=Chrome/73.0.3683.86");
+    options.AddArguments(
+      "no-sandbox",
+      "headless",
+      "disable-gpu",
+      "disable-logging",
+      "disable-dev-shm-usage",
+      "window-size=1920,1080",
+      "disable-extensions",
+      "log-level=OFF",
+      "--user-agent=Chrome/73.0.3683.86",
+      "output=/dev/null"
+    );
 
     var driver = new ChromeDriver(service, options);
     return driver;
