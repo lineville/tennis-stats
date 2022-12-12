8ab85124e9705c23a90d4c2062fcada5e5aa095d
Liam Neville <lineville@github.com>
2022-12-11 22:00:37 -0800

    format

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -1,17 +1,18 @@
 #! /usr/bin/env node
 
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const sendGridMailClient = require("@sendgrid/mail");
 
-const msg = {
-    to: 'liamgneville@gmail.com',
-    from: 'ustascraper@gmail.com',
-    subject: 'USTA Rankings Update',
-    text: process.env.EMAIL_BODY,
-    html: `<p>${process.env.EMAIL_BODY}</p>`,
+sendGridMailClient.setApiKey(process.env.SENDGRID_API_KEY);
+
+const message = {
+  to: "liamgneville@gmail.com",
+  from: "ustascraper@gmail.com",
+  subject: "USTA Rankings Update",
+  text: process.env.EMAIL_BODY,
+  html: `<p>${process.env.EMAIL_BODY}</p>`,
 };
 
-sgMail
-    .send(msg)
-    .then(() => console.log('Mail sent successfully'))
-    .catch(error => console.error(error.toString()));
+sendGridMailClient
+  .send(message)
+  .then(() => console.log("Mail sent successfully"))
+  .catch((error) => console.error(error.toString()));
