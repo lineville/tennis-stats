181aefd2889fb5eba3b2f26b9bfd089150b9f45b
Liam Neville <lineville@github.com>
2022-12-13 15:29:08 -0800

    md -> html

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -1,15 +1,18 @@
 #! /usr/bin/env node
 
 const sendGridMailClient = require("@sendgrid/mail");
-
 sendGridMailClient.setApiKey(process.env.SENDGRID_API_KEY);
+
+const showdown = require("showdown");
+const converter = new showdown.Converter();
+const html = converter.makeHtml(process.env.EMAIL_BODY);
 
 const message = {
   to: process.env.TO_EMAIL,
   from: process.env.FROM_EMAIL,
   subject: "USTA Rankings Update",
   text: process.env.EMAIL_BODY,
-  html: `<p>${process.env.EMAIL_BODY}</p>`,
+  html: html,
 };
 
 sendGridMailClient
