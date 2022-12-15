4f02aae1722d25ca3d19245743d9919baffe40a9
lineville <lineville@github.com>
2022-12-14 19:31:10 -0800

    trim selenium output

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -3,12 +3,21 @@
 const sendGridMailClient = require("@sendgrid/mail");
 sendGridMailClient.setApiKey(process.env.SENDGRID_API_KEY);
 
+const trimmedEmailBody = (str) => {
+  if (str.startsWith("ChromeDriver was started successfully.")) {
+    return str.split("\n").slice(1).join("\n");
+  } else {
+    return str;
+  }
+}
+
+
 const message = {
   to: process.env.TO_EMAIL,
   from: process.env.FROM_EMAIL,
   subject: "USTA Rankings Update",
-  text: process.env.EMAIL_BODY,
-  html: process.env.EMAIL_BODY,
+  text: trimmedEmailBody(process.env.EMAIL_BODY),
+  html: trimmedEmailBody(process.env.EMAIL_BODY),
 };
 
 sendGridMailClient
