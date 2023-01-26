bcecd462163039d7c4165c85aae860443c71a20f
Liam Neville <lineville@github.com>
2023-01-26 14:51:04 -0800

    add test

--- USTACLI.Tests/UnitTests.cs
+++ USTACLI.Tests/UnitTests.cs
@@ -1,5 +1,6 @@
 namespace USTACLI.Tests;
 
+using Xunit;
 using Microsoft.Extensions.Configuration;
 
 public class UnitTests
@@ -30,5 +31,30 @@ public class UnitTests
   {
     var service = Program.CreateChromeDriverService();
     Assert.NotNull(service);
+  }
+
+  [Fact]
+  public void TestScrapePlayerRanking()
+  {
+    var options = new CLIOptions()
+    {
+      Name = "Liam Neville",
+      Format = MatchFormat.SINGLES,
+      Gender = Gender.M,
+      Level = "4.0",
+      Section = "Northern California"
+    };
+
+    var config = new ConfigurationBuilder()
+        .SetBasePath(AppContext.BaseDirectory)
+        .AddJsonFile("appsettings.Test.json", false, true)
+        .Build();
+
+    var driver = Program.CreateChromeDriverService();
+    var url = Program.BuildUSTARankingURL(options, config);
+
+    var player = Program.ScrapePlayerRanking(driver, url, config, options);
+
+    Assert.NotNull(player);
   }
 }
