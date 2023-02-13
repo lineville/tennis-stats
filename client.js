4e834b59c0bc4d39de5e3818c4c43fd3c7c99c98
Liam Neville <lineville@github.com>
2023-02-13 15:48:38 -0800

    wip

--- client.js
+++ client.js
@@ -13,22 +13,8 @@ client.on("error", function (err) {
 
 console.log("Redis client connected");
 
-client.quit();
+const keys = await client.sendCommand(["keys","*"])
 
-// // Sets the key "octocat" to a value of "Mona the octocat"
-// redisClient.set("octocat", "Mona the Octocat", redis.print);
-// // Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
-// redisClient.hset("species", "octocat", "Cat and Octopus", redis.print);
-// // Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
-// redisClient.hset("species", "dinotocat", "Dinosaur and Octopus", redis.print);
-// // Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
-// redisClient.hset(["species", "robotocat", "Cat and Robot"], redis.print);
-// // Gets all fields in "species" key
+console.log(keys)
 
-// redisClient.hkeys("species", function (err, replies) {
-//   console.log(replies.length + " replies:");
-//   replies.forEach(function (reply, i) {
-//     console.log("    " + i + ": " + reply);
-//   });
-//   redisClient.quit();
-// });
+client.disconnect();
