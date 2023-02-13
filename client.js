59a72b0777017219c2b2fbc1c8cdd080240abeec
Liam Neville <lineville@github.com>
2023-02-13 15:54:34 -0800

    wip

--- client.js
+++ client.js
@@ -3,11 +3,13 @@ const redis = require("redis");
 const client = redis.createClient({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
-  user: process.env.REDIS_USER,
 });
-client.connect().then(() => {
+
+client.auth(process.env.REDIS_PASSWORD);
+
+client.on("connect", () => {
   console.log("Connected to Redis");
+  
   client.sendCommand(["keys", "*"]).then((keys) => {
     console.log(keys);
   });
