5f84f438c171635f412221e606836908a3fb2f38
Liam Neville <lineville@github.com>
2023-02-13 18:20:34 -0800

    typo

--- client.js
+++ client.js
@@ -8,26 +8,26 @@ const client = redis.createClient({
   },
 });
 
-redisClient.on("error", function(err) {
+client.on("error", function(err) {
   console.log("Error " + err);
 });
 
 // Sets the key "octocat" to a value of "Mona the octocat"
-redisClient.set("octocat", "Mona the Octocat", redis.print);
+client.set("octocat", "Mona the Octocat", redis.print);
 // Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
-redisClient.hset("species", "octocat", "Cat and Octopus", redis.print);
+client.hset("species", "octocat", "Cat and Octopus", redis.print);
 // Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
-redisClient.hset("species", "dinotocat", "Dinosaur and Octopus", redis.print);
+client.hset("species", "dinotocat", "Dinosaur and Octopus", redis.print);
 // Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
-redisClient.hset(["species", "robotocat", "Cat and Robot"], redis.print);
+client.hset(["species", "robotocat", "Cat and Robot"], redis.print);
 // Gets all fields in "species" key
 
-redisClient.hkeys("species", function (err, replies) {
+client.hkeys("species", function (err, replies) {
   console.log(replies.length + " replies:");
   replies.forEach(function (reply, i) {
       console.log("    " + i + ": " + reply);
   });
-  redisClient.quit();
+  client.quit();
 });
 // client.on("error", (error) => {
 //   console.error(error);
