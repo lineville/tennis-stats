4ec7ddccfe7fc8953d22f036737fa6344c74bb69
Liam Neville <lineville@github.com>
2023-02-13 18:19:15 -0800

    sanity check

--- client.js
+++ client.js
@@ -8,14 +8,35 @@ const client = redis.createClient({
   },
 });
 
-client.on("error", (error) => {
-  console.error(error);
+redisClient.on("error", function(err) {
+  console.log("Error " + err);
 });
 
-client.on("connect", () => {
-  console.log("Connected to Redis");
+// Sets the key "octocat" to a value of "Mona the octocat"
+redisClient.set("octocat", "Mona the Octocat", redis.print);
+// Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
+redisClient.hset("species", "octocat", "Cat and Octopus", redis.print);
+// Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
+redisClient.hset("species", "dinotocat", "Dinosaur and Octopus", redis.print);
+// Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
+redisClient.hset(["species", "robotocat", "Cat and Robot"], redis.print);
+// Gets all fields in "species" key
 
-  client.sendCommand(["keys", "*"]).then((keys) => {
-    console.log(keys);
+redisClient.hkeys("species", function (err, replies) {
+  console.log(replies.length + " replies:");
+  replies.forEach(function (reply, i) {
+      console.log("    " + i + ": " + reply);
   });
+  redisClient.quit();
 });
+// client.on("error", (error) => {
+//   console.error(error);
+// });
+
+// client.on("connect", () => {
+//   console.log("Connected to Redis");
+
+//   client.sendCommand(["keys", "*"]).then((keys) => {
+//     console.log(keys);
+//   });
+// });
